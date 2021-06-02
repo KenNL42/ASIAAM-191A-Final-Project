d4420270de545c3f99a4645f90d84c0d940d3da8
KenNL42 <59517626+KenNL42@users.noreply.github.com>
2021-06-02 00:18:45 -0700

    add about nav

--- js/init.js
+++ js/init.js
@@ -229,28 +229,57 @@ function renderChart(chartData, title, divID){
 
 //--------------create modal-------------------//
 // Get the modal
-var modal = document.getElementById("reportModal");
+var reportModal = document.getElementById("reportModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("reportButton");
+var reportBtn = document.getElementById("reportButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var reportClose = document.getElementsByClassName("reportClose")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+reportBtn.onclick = function() {
+  reportModal.style.display = "block";
   loadChart(0);
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+reportClose.onclick = function() {
+  reportModal.style.display = "none";
+  aboutModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == reportModal) {
+    reportModal.style.display = "none";
+  }
+}
+
+//----------about section-------------
+// Get the modal
+var aboutModal = document.getElementById("aboutModal");
+
+// Get the button that opens the modal
+var aboutBtn = document.getElementById("aboutButton");
+
+// Get the <span> element that closes the modal
+var aboutClose = document.getElementsByClassName("aboutClose")[0];
+
+// When the user clicks on the button, open the modal
+aboutBtn.onclick = function() {
+  aboutModal.style.display = "block";
+  loadChart(0);
+}
+
+// When the user clicks on <span> (x), close the modal
+aboutClose.onclick = function() {
+  aboutModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == aboutModal) {
+    aboutModal.style.display = "none";
   }
 }
